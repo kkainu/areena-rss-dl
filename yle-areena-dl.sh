5bfdec473809cc87cd7f34fa8c5b33a4650e0e23
Kalle Kainu <kkainu@gmail.com>
2013-09-07 21:41:02 +0300

    initial scripts

---
+++ yle-areena-dl.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#################################################################################
+#
+#	Don't touch these
+#
+#################################################################################
+
+YLE_DL=/Applications/Areena-lataaja.app/Contents/Resources/yle-dl
+SHOW_DIR=$BASEDIR/$SHOW
+DOWNLOADED_FILE=$SHOW_DIR/downloaded.txt
+LOG_PREFIX="[AREENA-DOWNLOAD : $SHOW] "
+LAUNCH_CTL_FILE=~/Library/LaunchAgents/areena.$SHOW.plist
+
+if [ ! -f $SHOW_DIR ]; then
+	mkdir -p $SHOW_DIR
+fi
+
+if [ ! -f $LAUNCH_CTL_FILE ]; then
+cat << EOF > $LAUNCH_CTL_FILE
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>areena.$SHOW</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>$SCRIPT</string>
+    </array>
+    <key>StartInterval</key>
+    <integer>10</integer>
+</dict>
+</plist>
+EOF
+
+launchctl load $LAUNCH_CTL_FILE
+fi
+
+urls=$(curl --silent $URL | grep -o '<link>.*</link>' | sed 's|<link>\(.*\)</link>|\1|g' | grep '[0-9]$')
+
+for url in $urls
+do
+	if grep --quiet $url $DOWNLOADED_FILE; then
+  		echo "$LOG_PREFIX $url already downloaded" | logger
+	else
+  		echo "$LOG_PREFIX downloading $url" | logger
+  		$YLE_DL/yle-dl --rtmpdump $YLE_DL/rtmpdump --destdir "$SHOW_DIR" $url
+		echo $url >> $DOWNLOADED_FILE
+	fi
+done
