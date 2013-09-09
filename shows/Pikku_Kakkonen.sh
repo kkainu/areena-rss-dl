867bdcd86d390b7a80b4c8117b61acadff2c98a6
Kalle Kainu <kkainu@gmail.com>
2013-09-09 23:05:33 +0300

    start refactoring

---
+++ shows/Pikku_Kakkonen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SHOW="Pikku Kakkonen"
+URL="http://areena.yle.fi/api/search.rss?id=1792189&media=video&sisalto=ohjelmat"
--- yle-areena-dl.sh
+++ yle-areena-dl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BASE_DIR=~/Desktop/Areena
+
 #################################################################################
 #
 #	Don't touch these
@@ -7,11 +9,13 @@
 #################################################################################
 
 YLE_DL=/Applications/Areena-lataaja.app/Contents/Resources/yle-dl
-SHOW_DIR=$BASEDIR/$SHOW
-DOWNLOADED_FILE=$SHOW_DIR/downloaded.txt
-LAUNCH_CTL=areena.$SHOW
+SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P)
+SCRIPT_FILE=$SCRIPT_DIR/$(basename $0)
+LAUNCH_CTL=com.areena-dl
 LAUNCH_CTL_FILE=~/Library/LaunchAgents/$LAUNCH_CTL.plist
-LOG_FILE=$SHOW_DIR/areena-dl.log
+SHOWS_DIR=$SCRIPT_DIR/shows
+DOWNLOADED_FILE=$BASE_DIR/downloaded.txt
+LOG_FILE=$BASE_DIR/areena-dl.log
 
 function install {
 	if [ ! -f $LAUNCH_CTL_FILE ]; then
@@ -21,10 +25,10 @@ function install {
 		<plist version="1.0">
 		<dict>
 		    <key>Label</key>
-		    <string>areena.$SHOW</string>
+		    <string>$LAUNCH_CTL</string>
 		    <key>ProgramArguments</key>
 		    <array>
-		        <string>$SCRIPT</string>
+		        <string>$SCRIPT_FILE</string>
 		    </array>
 		    <key>StartInterval</key>
 		    <integer>3600</integer>
@@ -44,46 +48,67 @@ function uninstall {
 	rm $LAUNCH_CTL_FILE
 }
 
+function addshow {
+	title=$(curl --silent "$1" | grep -m 1 -o 'CDATA\[.*\]' | sed 's/.*\[\(.*\)\]\]/\1/')
+	script="$(sed 's/ /_/' <<< $title).sh"
+
+	cat <<- EOF > "$SHOWS_DIR/$script"
+		#!/bin/bash
+
+		SHOW="$title"
+		URL="$1"
+	EOF
+	chmod u+x "$SHOWS_DIR/$script"
+}
+
 function run {
-	if [ ! -f $SHOW_DIR ]; then
-		mkdir -p $SHOW_DIR
+	if [ ! -f "$BASE_DIR" ]; then
+		mkdir -p "$BASE_DIR"
 	fi
 
-	if [ ! -f $DOWNLOADED_FILE ]; then
-		touch $DOWNLOADED_FILE
+	if [ ! -f "$DOWNLOADED_FILE" ]; then
+		touch "$DOWNLOADED_FILE"
 	fi
-	
-	urls=$(curl --silent $URL | grep -o '<link>.*</link>' | sed 's|<link>\(.*\)</link>|\1|g' | grep '[0-9]$')
 
-	echo "$(date): checking for new episodes of $SHOW" >> $LOG_FILE
-
-	for url in $urls
+	for show in $(ls $SHOWS_DIR)
 	do
-		if grep --quiet $url $DOWNLOADED_FILE; then
-	  		echo "$url already downloaded"
-		else
-	  		echo "$(date): downloading $url" >> $LOG_FILE
-	  		$YLE_DL/yle-dl --rtmpdump $YLE_DL/rtmpdump --destdir "$SHOW_DIR" $url
-	  		if [ $? -eq 0 ]; then
-    			echo $url >> $DOWNLOADED_FILE
-			else
-    			echo "$(date): $url download failed" >> $LOG_FILE
-			fi
+		source "$SHOWS_DIR/$show"
+		
+		if [ ! -f "$BASE_DIR" ]; then
+			mkdir -p "$BASE_DIR/$SHOW"
 		fi
+
+		echo "$(date): checking for new episodes of $SHOW" >> "$LOG_FILE"
+		urls=$(curl --silent $URL | grep -o '<link>.*</link>' | sed 's|<link>\(.*\)</link>|\1|g' | grep '[0-9]$')
+		for url in $urls
+		do
+			if grep --quiet $url $DOWNLOADED_FILE; then
+		  		echo "$url already downloaded"
+			else
+		  		echo "$(date): downloading $url" >> "$LOG_FILE"
+		  		$YLE_DL/yle-dl --rtmpdump $YLE_DL/rtmpdump --destdir "$BASE_DIR/$SHOW" $url
+		  		if [ $? -eq 0 ]; then
+	    			echo $url >> "$DOWNLOADED_FILE"
+				else
+	    			echo "$(date): $url download failed" >> "$LOG_FILE"
+				fi
+			fi
+		done
 	done
 
-	echo "$(date): done." >> $LOG_FILE
+	echo "$(date): done." >> "$LOG_FILE"
 }
 
 function usage {
 	cat <<-ENDOFMESSAGE
 
-	Usage: $(basename "$0") [-i|-u|-h]
+	Usage: $(basename "$0") [-i|-u|-h|-a rss_url]
 
 	where
-	-i installs the periodically running new episode checker
-	-u uninstalls the episode checker
-	-h prints this message
+	-i              installs the periodically running new episode checker
+	-u              uninstalls the episode checker
+	-a rss_url      adds a new show using the shows rss feed url rss_url
+	-h              prints this message
 
 	ENDOFMESSAGE
 }
@@ -99,6 +124,13 @@ case "$1" in
 		;;
     -u | --uninstall)
     	uninstall
+		;;
+	-a | --addshow)
+		if [ "$#" -lt "2" ]; then
+    		usage
+    		exit 0
+		fi
+		addshow "$2"
 		;;
     -h | --help)
     	usage
